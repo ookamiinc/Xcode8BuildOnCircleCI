25e8a47f3051dc4ace8d65a311cf2a420359f314
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-09-23 15:27:05 +0900

    Initial Commit

---
+++ Xcode8BuildOnCircleCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode8BuildOnCircleCI
+//
+//  Created by Fumiya Nakamura on 9/23/16.
+//  Copyright © 2016 Fumiya Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
